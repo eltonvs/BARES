423d20dc38f73cd065d361c0fd1300093164ed4d
Elton Viana <eltonvs@outlook.com>
2016-04-23 17:01:57 -0300

    Added comments on doxygen style

--- include/headers.hpp
+++ include/headers.hpp
@@ -1,4 +1,10 @@
-// Copyright - 2016
+/*!
+ *  @file headers.hpp
+ *  @brief Class Headers
+ *  @copyright Copyright &copy; 2016. All rights reserved.
+ *
+ *  File with all Class Headers and prototypes
+ */
 
 #ifndef _headers_hpp_
 #define _headers_hpp_
