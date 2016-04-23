85c89539b1f56cb0d3816c1f1594842000bbe354
Elton Viana <eltonvs@outlook.com>
2016-04-23 16:48:10 -0300

    Added initial files

---
+++ include/headers.hpp
@@ -0,0 +1,6 @@
+// Copyright - 2016
+
+#ifndef _headers_hpp_
+#define _headers_hpp_
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*!
+ *  @mainpage BARES
+ *  @authors Elton de Souza Vieira
+ *  @date April, 2016
+ *  @copyright Copyright &copy; 2016. All rights reserved.
+ *  @version 1.0
+ *
+ *  @file main.cpp
+ *  @brief Main File
+ *
+ *  File with the main function
+ */
+
+
+#include <iostream>
+#include "headers.hpp"
+
+int main(int argc, char const *argv[]) {
+    // Code Here
+
+    return EXIT_SUCCESS;
+}
