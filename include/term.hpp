520cd6e1abd6f2edbbd321f58aeafe11f72e3594
Elton Viana <eltonvs@outlook.com>
2016-04-29 22:05:30 -0300

    Added some new methods to Term class

--- include/term.hpp
+++ include/term.hpp
@@ -22,7 +22,7 @@ class Term {
     /**
      * @brief Term Constructor
      * @param _term Receives the Term content
-     * @param _col Receives the Term collumn
+     * @param _col Receives the Term column
      *
      * Creates a Term
      */
@@ -34,6 +34,47 @@ class Term {
      * Destroy a Term
      */
     ~Term();
+
+    /**
+     * @brief Getter to m_col
+     * @return The Term col
+     *
+     * Get the Term col
+     */
+    int get_col();
+
+    /**
+     * @brief Verify if the term is an expression number
+     * @return True if is a number, False otherwise
+     *
+     * Verify if is a number
+     */
+    bool is_number();
+
+    /**
+     * @brief Verify if the Term is an expression operand
+     * @return True if is an operand, False otherwise
+     *
+     * Verify if is a operand
+     */
+    bool is_operand();
+
+    /**
+     * @brief Verify if the Term is an expression operand
+     * @return True if is an opening parenthesus, False if not
+     *
+     * Verify if is a opening parenthesis
+     */
+    bool is_opening_parenthesis();
+
+    /**
+     * @brief Verify if the Term is an coling parenthesis
+     * @return True if is a closing parenthesis, False if not
+     *
+     * Verify if is a closing parenthesis
+     */
+    bool is_closing_parenthesis();
+
 
  private:
     std::string m_term = "";  //<! The term value
--- include/term.inl
+++ include/term.inl
@@ -15,3 +15,34 @@ Term::Term(std::string _term, int _col) : m_term(_term), m_col(_col) {}
 Term::~Term() {
     m_term.clear();
 }
+
+// Getter Term Column
+int Term::get_col() {
+    return m_col;
+}
+
+// Verify if the term is a number
+bool Term::is_number() {
+    for (auto i(0u); i < m_term.size(); i++)
+        if (m_term[i] < 48 or m_term[i] > 57)
+            return false;
+    return true;
+}
+
+// Verify if the term is an operand
+bool Term::is_operand() {
+    if (m_term == "%" or m_term == "*" or m_term == "+" or
+        m_term == "-" or m_term == "/" or m_term == "^")
+        return true;
+    return false;
+}
+
+// Verify if the term is an opening parenthesis
+bool Term::is_opening_parenthesis() {
+    return m_term == "(";
+}
+
+// Verify if the term is a closing parenthesis
+bool Term::is_closing_parenthesis() {
+    return m_term == ")";
+}
