a0acd9c27a71e1d2efc1a98d3ec1238dc6fa14a9
Elton Viana <eltonvs@outlook.com>
2016-05-05 09:34:00 -0300

    Added documentation to Term struct

--- include/term.hpp
+++ include/term.hpp
@@ -1,6 +1,6 @@
 /*!
  *  @file term.hpp
- *  @brief Term struct Header
+ *  @brief Term struct Declaration
  *  @copyright Copyright &copy; 2016. All rights reserved.
  *
  *  File with the Term Struct header
@@ -12,14 +12,31 @@
 #include <string>
 #include "queue.hpp"
 
+/**
+ * @brief The Term struct
+ *
+ * The Term struct implementation
+ */
 struct Term {
-    std::string value = "";
-    int col = -1;
-    bool is_unary = false;
+    std::string value = "";  //!< The term value
+    int col = -1;            //!< The term column
+    bool is_unary = false;   //!< Flag to indicate if is a unary operator
 
+    /**
+     * @brief Term Constructor
+     * @param _val The Term value (to fit value variable)
+     * @param _col The Term column (to fit col variable)
+     *
+     * The Term Constructor function
+     */
     explicit Term(std::string _val = "", int _col = -1) : value(_val), col(_col) {}
 };
 
+/**
+ * @brief Overload << operator to Terms
+ * @param _os The std::ostream
+ * @param _term The Term to be showed
+ */
 std::ostream &operator<<(std::ostream &_os, const Term &_term) {
     return _os << "\"" << _term.value << "\"";
 }
